f0fa4d68c5f297b7069ffa06cbd3c51bdd2bb8c6
MayMyatNoeSwe <myatnoen395@gmail.com>
2024-11-07 21:38:10 +0630

    PHP-Project

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php 
+include './layouts/header.php';
+?>
+
+<div class="container-xxl bg-white p-0">
+
+
+    <!-- Spinner Start -->
+    <?php include('./layouts/spinner.php'); ?>
+    <!-- Spinner End -->
+
+    <!-- Navbar Start -->
+    <?php include('./layouts/navbar.php'); ?>
+
+    <!-- Navbar End -->
+
+
+    <!-- Carousel Start -->
+    <?php include('./layouts/carousel.php'); ?>
+    <!-- Carousel End -->
+
+
+    <!-- Facilities Start -->
+    <?php include('./layouts/facilities.php'); ?>
+    <!-- Facilities End -->
+
+
+    <!-- About Start -->
+    <?php include('./layouts/about.php'); ?>
+    <!-- About End -->
+
+
+    <!-- Call To Action Start -->
+    <?php include('./layouts/callToAction.php'); ?>
+    <!-- Call To Action End -->
+
+
+    <!-- Classes Start -->
+    <?php include('./layouts/classes.php'); ?>
+    <!-- Classes End -->
+
+
+    <!-- Appointment Start -->
+    <?php include('./layouts/appointment.php'); ?>
+    <!-- Appointment End -->
+
+
+    <!-- Team Start -->
+    <?php include('./layouts/team.php'); ?>
+    <!-- Team End -->
+
+
+    <!-- Testimonial Start -->
+    <?php include('./layouts/testimonial.php'); ?>
+    <!-- Testimonial End -->
+
+
+    <!-- Footer Start -->
+    <?php include  './layouts/footer.php'; ?>
